aef7d1c2cc3a8a3bff9613363764bfdce903ad10
ethernet32 <ethan.ralphs@ucas-edu.net>
2024-11-19 11:37:52 -0700

    made the UI and finished the part 4

--- dessertshop.py
+++ dessertshop.py
@@ -7,6 +7,30 @@ from dessert import (
 )
 from receipt import *
 
+class DessertShop():
+    def user_prompt_cookie(self):
+        type = input("Enter the type of cookie: ")
+        quantity = input("Enter the quantity purchased: ")
+        price = input("Enter the price per dozen: ")
+        return Cookie(type,int(quantity),float(price))
+    def user_prompt_candy(self):
+        type = input("Enter the type of candy: ")
+        quantity = input("Enter the amount in pounds purchased: ")
+        price = input("Enter the price per pound: ")
+        return Candy(type,int(quantity),float(price))
+    def user_prompt_icecream(self):
+        type = input("Enter the type of ice cream: ")
+        quantity = input("Enter the number of scoops: ")
+        price = input("Enter the price per scoop: ")
+        return Candy(type,int(quantity),float(price))
+    def user_prompt_sundae(self):
+        type = input("Enter the type of ice cream: ")
+        quantity = input("Enter the number of scoops: ")
+        price = input("Enter the price per scoop: ")
+        topping = input("Enter the topping: ")
+        tprice = input("Enter the price for the topping: ")
+        return Candy(type,int(quantity),float(price), topping, float(tprice))
+
 class Order():
     def __init__(self):
         self.order=[]
@@ -28,25 +52,57 @@ class Order():
 
 
 def main():
-    order1 = Order()
-    order1.add(Candy("Candy Corn", 1.5, .25))
-    order1.add(Candy("Gummy Bears", .25, .35))
-    order1.add( Cookie("Chocolate Chip", 6, 3.99))
-    order1.add(IceCream("Pistachio", 2, .79))
-    order1.add(Sundae("Vanilla", 3, .69, "Hot Fudge", 1.29))
-    order1.add(Cookie("Oatmeal Raisin", 2, 3.45))
-    total_item = 0
-    for item in order1.order:
-        print(item)
-        total_item += 1
-    print(f"Total number of items in order: {total_item}")
+    shop = DessertShop()
+    order = Order()
+    '''
+    order.add(Candy('Candy Corn', 1.5, 0.25))
+    order.add(Candy('Gummy Bears', 0.25, 0.35))
+    order.add(Cookie('Chocolate Chip', 6, 3.99))
+    order.add(IceCream('Pistachio', 2, 0.79))
+    order.add(Sundae('Vanilla', 3, 0.69, 'Hot Fudge', 1.29))
+    order.add(Cookie('Oatmeal Raisin', 2, 3.45))
+    '''
+    # boolean done = false
+    done: bool = False
+    # build the prompt string once
+    prompt = '\n'.join([ '\n',
+            '1: Candy',
+            '2: Cookie',
+            '3: Ice Cream',
+            '4: Sunday',
+            '\nWhat would you like to add to the order? (1-4, Enter for done): '
+        ])
+    while not done:
+        choice = input(prompt)
+        match choice:
+            case '':
+                done = True
+            case '1':
+                item = shop.user_prompt_candy()
+                order.add(item)
+                print(f'{item.name} has been added to your order.')
+            case '2':
+                item = shop.user_prompt_cookie()
+                order.add(item)
+                print(f'{item.name} has been added to your order.')
+            case '3':
+                item = shop.user_prompt_icecream()
+                order.add(item)
+                print(f'{item.name} has been added to your order.')
+            case '4':
+                item = shop.user_prompt_sundae()
+                order.add(item)
+                print(f'{item.name} has been added to your order.')
+            case _:
+                print('Invalid response: Please enter a choice from the menu (1-4) or Enter')
+    print()
     DATA = [ 
         [ "Name" , "Price", "Tax"]
     ]
-    for item in order1.order:
+    for item in order.order:
         DATA.append([item.name,"$"+str(round(item.calculate_cost(),2)), "$"+str(round(item.calculate_tax(),2)) ])
-    DATA.append(["Subtotal", "$"+str(round(order1.order_cost(),2)), "$"+str(round(order1.order_tax(),2))])
-    DATA.append(["Total", "", "$"+str(round(order1.order_cost()+order1.order_tax(),2))]) 
-    DATA.append(["Total items in the order","", str(total_item), ])
+    DATA.append(["Subtotal", "$"+str(round(order.order_cost(),2)), "$"+str(round(order.order_tax(),2))])
+    DATA.append(["Total", "", "$"+str(round(order.order_cost()+order.order_tax(),2))]) 
+    DATA.append(["Total items in the order","", len(order), ])
     make_receipt(DATA,"reciept.pdf")
 main()
